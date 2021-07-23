b74dd8fa15b72892e8b5e621ed03670077233495
samitin <samitin.e@gmail.com>
2021-07-24 00:35:46 +1100

    5.a) Сформировать data class с двумя свойствами и вывести их на экран приложения.

--- app/src/main/java/ru/samitin/lesson1/MainActivity.kt
+++ app/src/main/java/ru/samitin/lesson1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import ru.samitin.lesson1.data.KeyValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +21,10 @@ class MainActivity : AppCompatActivity() {
                 textview.setText(text+i+"!")
             }
         } )
+        val keyValue=KeyValue("Ключь","Значение")
+        val textKey:TextView=findViewById(R.id.text_key);
+        textKey.setText(textKey.text.toString()+keyValue.key)
+        val textValue:TextView=findViewById(R.id.text_value)
+        textValue.setText(textValue.text.toString()+keyValue.value)
     }
 }
---
+++ app/src/main/java/ru/samitin/lesson1/data/KeyValue.kt
@@ -0,0 +1,3 @@
+package ru.samitin.lesson1.data
+
+data class KeyValue(var key:String="key",var value:String="value")
